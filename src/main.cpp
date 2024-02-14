b011f46b371c757e513cab3c7a574050fccb999f
Gerard <gerardfabrego@gmail.com>
2024-02-14 11:06:10 +0100

    Extract screen width and height as consts

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
 void checkShaderCompilationStatus(unsigned int shader, std::string error_message)
 {
     int success;
@@ -64,7 +67,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
